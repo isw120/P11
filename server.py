30d9698dd66107af4a9e779e6466d1f254a2ae98
isw120 <row120@hotmail.fr>
2022-01-29 19:45:48 +0100

    First feature added

--- server.py
+++ server.py
@@ -62,8 +62,9 @@ def index():
 def showSummary():
         try:
             club = checkEmail(request.form['email'])
+            other_clubs = [c for c in clubs if c['email'] != request.form['email']]
             all_competitions = checkCompetitionsDates()
-            return render_template('welcome.html', club=club, competitions=all_competitions)
+            return render_template('welcome.html', club=club, clubs=other_clubs, competitions=all_competitions)
         except IndexError:
             return render_template('index.html', error="Sorry, that email wasn't found.")
 
@@ -72,28 +73,29 @@ def showSummary():
 def book(competition,club, error=None):
     foundClub = [c for c in clubs if c['name'] == club][0]
     foundCompetition = [c for c in competitions if c['name'] == competition][0]
-    print(foundCompetition)
+    other_clubs = [c for c in clubs if c['name'] != club]
     if error is not None:
         return render_template('booking.html',club=foundClub,competition=foundCompetition, error=error)
     if foundCompetition['status'] == 'invalid':
         flash("The competition " + str(foundCompetition['name']) + " is invalid.")
-        return render_template('welcome.html', club=foundClub, competitions=competitions)
+        return render_template('welcome.html', club=foundClub, clubs=other_clubs, competitions=competitions)
     if foundCompetition['numberOfPlaces'] == 0:
         flash("The competition " + str(foundCompetition['name']) + " is completed.")
-        return render_template('welcome.html', club=foundClub, competitions=competitions)
+        return render_template('welcome.html', club=foundClub, clubs=other_clubs, competitions=competitions)
     if foundClub and foundCompetition:
         return render_template('booking.html', club=foundClub, competition=foundCompetition)
     else:
         flash("Something went wrong-please try again")
-        return render_template('welcome.html', club=foundClub, competitions=competitions)
+        return render_template('welcome.html', club=foundClub, clubs=other_clubs, competitions=competitions)
 
 
 @app.route('/purchasePlaces',methods=['POST'])
 def purchasePlaces():
     check, club, competition = checkAvailablePoints(request.form['competition'], request.form['club'], request.form['places'])
+    other_clubs = [c for c in clubs if c['name'] != request.form['club']]
     if check is False:
         flash('Great-booking complete!')
-        return render_template('welcome.html', club=club, competitions=competitions)
+        return render_template('welcome.html', club=club, clubs=other_clubs, competitions=competitions)
     else:
         return book(competition, club, "You dont have enough points")
 
